2cd7de5dc751f30e5e2f3b56249c2a5baa5a2ccd
Slin <210841740+Ph1L-s@users.noreply.github.com>
2025-09-12 12:58:23 +0200

    Responisve 50% done..
    - Added responsive to all sections.

--- Portfolio/src/app/components/nav/nav.ts
+++ Portfolio/src/app/components/nav/nav.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './nav.html',
   styleUrl: './nav.scss'
 })
 export class Nav {
   selectedLanguage = 'EN';
+  isMenuOpen = false;
 
   selectLanguage(lang: string) {
     this.selectedLanguage = lang;
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    
+    if (this.isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+    document.body.style.overflow = '';
   }
 }
