d8a828a7b199bc9adea91605f66a6385453e1046
Ph1L-s <210841740+Ph1L-s@users.noreply.github.com>
2025-11-20 20:26:03 +0100

    Fix image paths to use absolute paths with correct casing

--- Portfolio/src/app/data/projects.data.ts
+++ Portfolio/src/app/data/projects.data.ts
@@ -15,13 +15,13 @@ export const PROJECTS: Projects[] = [
     nameKey: 'Join',
     descriptionKey: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
     tech: [
-      { tech: 'Angular', image: 'designs/images/languages/angular.svg' },
-      { tech: 'TypeScript', image: 'designs/images/languages/type_script.svg' },
-      { tech: 'HTML', image: 'designs/images/languages/html.svg' },
-      { tech: 'CSS', image: 'designs/images/languages/stylesheet.svg' },
-      { tech: 'Firebase', image: 'designs/images/languages/firebase.svg' }
+      { tech: 'Angular', image: '/designs/Images/languages/angular.svg' },
+      { tech: 'TypeScript', image: '/designs/Images/languages/type_script.svg' },
+      { tech: 'HTML', image: '/designs/Images/languages/html.svg' },
+      { tech: 'CSS', image: '/designs/Images/languages/stylesheet.svg' },
+      { tech: 'Firebase', image: '/designs/Images/languages/firebase.svg' }
     ],
-    image: 'designs/images/featured_projects/join.svg',
+    image: '/designs/Images/featured_projects/join.svg',
     github: 'https://github.com/Ph1L-s/Join-Project',
     liveTest: 'https://www.04-25-aw-join-1.developerakademie.net/angular-projects/join/login'
   },
@@ -29,11 +29,11 @@ export const PROJECTS: Projects[] = [
     nameKey: 'El Pollo Loco',
     descriptionKey: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
     tech: [
-      { tech: 'HTML', image: 'designs/images/languages/html.svg' },
-      { tech: 'CSS', image: 'designs/images/languages/stylesheet.svg' },
-      { tech: 'JavaScript', image: 'designs/images/languages/java_script.svg' }
+      { tech: 'HTML', image: '/designs/Images/languages/html.svg' },
+      { tech: 'CSS', image: '/designs/Images/languages/stylesheet.svg' },
+      { tech: 'JavaScript', image: '/designs/Images/languages/java_script.svg' }
     ],
-    image: 'designs/images/featured_projects/pollo.svg',
+    image: '/designs/Images/featured_projects/pollo.svg',
     github: 'https://github.com/Ph1L-s/Pollo-Loco',
     liveTest: 'https://phillip-schulze.developerakademie.net/module%2012/EPL/index.html'
   },
@@ -41,11 +41,11 @@ export const PROJECTS: Projects[] = [
     nameKey: 'DA Bubble',
     descriptionKey: 'Slack-inspired chat application with real-time messaging, channels, and direct messages. Built with Angular and Firebase.',
     tech: [
-      { tech: 'Angular', image: 'designs/images/languages/angular.svg' },
-      { tech: 'Firebase', image: 'designs/images/languages/firebase.svg' },
-      { tech: 'TypeScript', image: 'designs/images/languages/type_script.svg' }
+      { tech: 'Angular', image: '/designs/Images/languages/angular.svg' },
+      { tech: 'Firebase', image: '/designs/Images/languages/firebase.svg' },
+      { tech: 'TypeScript', image: '/designs/Images/languages/type_script.svg' }
     ],
-    image: 'designs/images/featured_projects/da_bubble.svg',
+    image: '/designs/Images/featured_projects/da_bubble.svg',
     github: 'https://github.com/Ph1L-s',
     liveTest: 'https://da-bubble-demo.com'
   }
